bc01b3f7b5efa55d5e408539a893239efda42b08
omniroot <omnirootofc@gmail.com>
2025-07-31 21:53:28 +0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,16 @@
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { RouterProvider } from 'react-router-dom';
+import { MantineProvider } from '@mantine/core';
+import { router } from '@/router.js';
+import { theme } from '@/theme.js';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+import '@mantine/core/styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <MantineProvider theme={theme}>
+      <RouterProvider router={router} />
+    </MantineProvider>
+  </StrictMode>
+);
--- src/theme.ts
+++ src/theme.ts
@@ -2,4 +2,9 @@ import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
   /** Put your mantine theme override here */
+  colors: {
+    primary: ["#AB6F2A", "#AB6F2A", "#AB6F2A", "#AB6F2A", "#AB6F2A", "#AB6F2A", "#AB6F2A", "#AB6F2A", "#AB6F2A", "#AB6F2A", "#AB6F2A"]
+  },1
+
+  primaryColor: "primary",
 });
